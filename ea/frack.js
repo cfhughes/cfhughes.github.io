b4be4c1b0cdbe4ce87abece13d50ef2a5212e0a1
Chris Hughes <chris@speedycomputing.com>
2015-12-03 15:53:24 -0700

    Final Version

--- ea/frack.js
+++ ea/frack.js
@@ -1,9 +1,12 @@
 var pages= ['facebook','reddit','nytimes','ebay','okcupid','google','youtube','craigslist','apple','amazon'];
 var num = 10;
+var p1;
 var paths = [];
+var loaded=false;
 $(function(){
 
     var data = [];
+
     var init = function(){
 
         paths = [];
@@ -19,6 +22,8 @@ $(function(){
             data[0].push(hist);
         });
 
+        //console.log("Paths: "+paths.length);
+
         for(var i = 1;i<50;i++){
             var hist = [];
             var factor = Math.pow(i,3)/5;
@@ -32,32 +37,67 @@ $(function(){
                 for (var k = 0;k<data[i-1][j].length;k++){
                     hist[j][k] = [];
                     hist[j][k][0] = data[i-1][j][k][0] + (Math.random() * 12) - 6;
-                    
                     hist[j][k][1] = data[i-1][j][k][1] + (Math.random() * factor) - d;
                 }
+
+                //$(paths[j]).show();
             }
             //console.log("Entry added with "+hist.length+" entries");
             data.push(hist);
         }
-
+        $("body").css('cursor','none');
+        loaded = true;
     };
 
-    init();
+    //******Assemble******
 
-    var prevx = 0;
-    var diff = 0;
+    var enable_assemble = function(){
+        $("#start").hide();
+        $("#main").show();
+        for (var k = 0; k < paths.length; k++) {
+            var segments = paths[k].pathSegList;
+            for (var i = 0; i < segments.length; i++) {
+                segments.getItem(i).y = data[0][k][i][1];
+                segments.getItem(i).x = data[0][k][i][0];
+            }
+        }
+        p1=0;
+        var disassemble = window.setInterval(function(){
+            for (var p2 = p1;p2<paths.length;p2+=10){
+                $(paths[p2]).hide();
+            }
+            if (p1 >= 9){
+                window.clearInterval(disassemble);
+                p1 = 0;
+                var assemble = window.setInterval(function(){
+                    for (var p2 = p1;p2<paths.length;p2+=10){
+                        $(paths[p2]).show();
+                    }
+                    if (p1 >= 9){
+                        window.clearInterval(assemble);
+                        $("#main").hide();
+                        $("#start").show();
+                    }
+                    p1++;
+                },400);
+            }else {
+                p1++;
+            }
+        },400);
+    };
 
-    var update = function(){
-        var posX = Math.floor(currentMousePos.x*50/window.innerWidth);
+    //******Fracture******
+    var update = function () {
+        var posX = Math.floor(currentMousePos.x * 50 / window.innerWidth);
         if (Math.abs(posX - prevx) > diff) {
             diff++;
         } else if (diff > 0) {
             diff--;
         }
-        if (diff == 0){
+        if (diff == 0) {
             $("#main").hide();
             $("#start").show();
-        }else {
+        } else {
             $("#start").hide();
             $("#main").show();
             //console.log("Position: "+diff);
@@ -70,25 +110,22 @@ $(function(){
             }
         }
         prevx = posX;
-    }
+    };
+    var diff;
+    var prevx = 0;
+    var updateI;
+    var enable_fracture = function() {
+        window.clearInterval(chaosI);
+        $("#main").hide();
+        $("#start").show();
 
-    var currentMousePos = { x: -1, y: -1 };
-    $(document).mousemove(function(event) {
-        currentMousePos.x = event.pageX;
-        currentMousePos.y = event.pageY;
-    });
+        diff = 0;
+        updateI = window.setInterval(update, 100);
 
-    var updateI = window.setInterval(update, 100);
-    var c = 0;
-    $("body").click(function(){
-        c++;
-        if (c == num)c=0;
+    };
 
-        $("#start").attr('src',"src/"+pages[c]+".png");
-        $("#main").load("src/"+pages[c]+".svg",function(){
-            init();
-        })
-    });
+    //******Chaos******
+
     var p = 0;
     var chaos = function(){
         for (var k = 0; k < paths.length; k++) {
@@ -98,32 +135,73 @@ $(function(){
                 segments.getItem(i).x+=(Math.random() * .8) - .4;
             }
         }
-    }
+    };
     var chaosI;
-    $(document).keypress(function(e) {
-      if(e.which == 48) {
-        // 0 pressed
-        if (p == 0){
-            p = 1;
-            window.clearInterval(updateI);
-            $("#start").hide();
-            $("#main").show();
-            for (var k = 0; k < paths.length; k++) {
-                var segments = paths[k].pathSegList;
-                for (var i = 0; i < segments.length; i++) {
-                    segments.getItem(i).y = data[0][k][i][1];
-                    segments.getItem(i).x = data[0][k][i][0];
-                }
+    var enable_chaos = function(){
+        $("#start").hide();
+        $("#main").show();
+        for (var k = 0; k < paths.length; k++) {
+            var segments = paths[k].pathSegList;
+            for (var i = 0; i < segments.length; i++) {
+                segments.getItem(i).y = data[0][k][i][1];
+                segments.getItem(i).x = data[0][k][i][0];
             }
-	    chaosI = window.setInterval(chaos, 100);
-	}else{
-            p = 0;
-            window.clearInterval(chaosI);
-            $("#main").hide();
-            $("#start").show();
-            updateI = window.setInterval(update, 100);
         }
-      }
+        chaosI = window.setInterval(chaos, 100);
+
+    };
+
+    //*******Reset******
+    var reset = function(){
+        window.clearInterval(chaosI);
+        window.clearInterval(updateI);
+    };
+
+    var start = Math.floor(Math.random()*num);
+    var c = start;
+    $("body").css('cursor', 'wait');
+    $("#start").attr('src',"src/"+pages[c]+".png");
+    $("#main").load("src/"+pages[c]+".svg",function(){
+        init();
+    });
+    var mode = 0;
+    $("body").click(function(){
+        //Don't change while loading
+        if (loaded) {
+            if (mode == 6) {
+                reset();
+                $("#main").hide();
+                $("#start").show();
+                c++;
+                if (c == num)c = 0;
+                if (c == start){
+                    //Stream
+                    window.location.href = "https://www.google.com/";
+                }
+                $("body").css('cursor', 'wait');
+                loaded = false;
+                $("#start").attr('src', "src/" + pages[c] + ".png");
+                $("#main").load("src/" + pages[c] + ".svg", function () {
+                    init();
+                });
+            } else if (mode % 3 == 0) {
+                reset();
+                enable_assemble();
+            } else if (mode % 3 == 1) {
+                enable_chaos();
+            } else if (mode % 3 == 2) {
+                enable_fracture();
+            }
+            mode++;
+            if (mode == 7)mode = 0;
+        }
+    });
+
+    //*****Mouse Tracking*****
+    var currentMousePos = { x: -1, y: -1 };
+    $(document).mousemove(function(event) {
+        currentMousePos.x = event.pageX;
+        currentMousePos.y = event.pageY;
     });
 
 });
